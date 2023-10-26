517fdb382caf15d65519f00dd98ecb995bf90483
Wito De Greef <witodegreef@Witos-MacBook-Pro-2.local>
2023-10-26 10:53:57 +0200

    Updated deploy config

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+  
+   // Add basePath
+    basePath: '/buzz_quiz_soundboard',
+};
+
+module.exports = nextConfig;
