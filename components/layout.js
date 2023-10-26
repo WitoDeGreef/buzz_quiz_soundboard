61adfec60265a9d530a1fe1c954981be6b262fa7
Wito De Greef <witodegreef@Witos-MacBook-Pro-2.local>
2023-10-26 10:30:09 +0200

    Possible fixes

--- components/layout.js
+++ components/layout.js
@@ -9,11 +9,8 @@ export default function Layout({children}) {
             <Head>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-                <meta name="description" content="" />
-                <meta name="author" content="" />
                 <title>Buzz Quiz Soundboard</title>
                 <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
             </Head>
             <main>
                 <Header />
---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+ 
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
