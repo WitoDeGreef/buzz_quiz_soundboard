0db5471754b3a2409e1d89e007ea5ac8752037aa
Wito De Greef <witodegreef@Witos-MacBook-Pro-2.local>
2023-10-26 12:19:50 +0200

    Centered detail page clickable area

--- pages/[soundName].js
+++ pages/[soundName].js
@@ -25,7 +25,7 @@ export default function Detail() {
         <section className="py-5">
             <div className="container px-4 px-lg-5 my-5">
                 <div className="row gx-4 gx-lg-5 align-items-center">
-                    <div className="col-md-12">
+                    <div className="col-md-12" style={{ display: "flex", flexDirection: "column", justifyContent: "center"}}>
                         <a className="btn" >
                             <img className="card-img-top mb-5 mb-md-0" src="https://dummyimage.com/600x700/dee2e6/6c757d.jpg" alt="..." onClick={playSound} />
                         </a>
